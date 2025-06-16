3d4899a45de5ba643f4c0675e2af4e9bcab334f7
runwezt <amarikwat@gmail.com>
2025-06-16 23:38:25 +0100

    fix firebase issue

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,12 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBiA31-0TemOLfvAWAWGC--2DFSLZQ-Mcw",
   authDomain: "marketing-program-cf060.firebaseapp.com",
   projectId: "marketing-program-cf060",
-  storageBucket: "marketing-program-cf060.firebasestorage.app",
+  storageBucket: "marketing-program-cf060.appspot.com",
   messagingSenderId: "360799407774",
   appId: "1:360799407774:web:57be443cf0a4a7519bf58f",
   measurementId: "G-ZHKJQ66CZ7"
@@ -14,4 +14,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
- export const auth = getAuth(app)
+export const auth = getAuth(app);
